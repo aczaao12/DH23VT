cf3d1f992da2f0c737d25b3aca45a346eeff1c75
aczaao12 <aczaao12@gmail.com>
2025-08-21 13:52:31 +0000

    style: Thêm css cho loginPgae

--- src/pages/LoginPage/LoginView.jsx
+++ src/pages/LoginPage/LoginView.jsx
@@ -1,5 +1,6 @@
 import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 import { auth } from "../../firebase";
+import './LoginView.css'; // Import the new CSS file
 
 const LoginView = () => {
   const handleGoogleLogin = () => {
@@ -8,9 +9,12 @@ const LoginView = () => {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={handleGoogleLogin}>Sign in with Google</button>
+    <div className="login-container"> {/* Add a container for full page styling */}
+      <div className="login-card"> {/* Add a card for the login form */}
+        <h1>Login</h1>
+        <p>Hãy dùng mail sinh viên để đăng nhập nhé!!!</p> {/* Added text */}
+        <button onClick={handleGoogleLogin}>Sign in with Google</button>
+      </div>
     </div>
   );
 };
